54b30a6adcce98f4ca61ebbcc10fa040f4b8e300
Ivan Tumanov <vrust00@yandex.ru>
2026-06-28 23:24:32 +0400

    link images in menu

--- js/home-btn.js
+++ js/home-btn.js
@@ -3,12 +3,18 @@ const homebg = document.querySelector('.home-bg');
 const blocks = document.querySelectorAll('.header, .main');
 
 btn.addEventListener('click', () => {
+   
+   document.documentElement.style.overflow = 'auto';
+   document.body.style.overflow = 'auto';
+   document.body.style.height = 'auto';
+   document.body.style.width = 'auto';
+
+   
    homebg.classList.add('slide');
    blocks.forEach(block => block.classList.add('unslide'));
+
    
-   
-   homebg.addEventListener('transitionend', ()=>{
-      homebg.style.display = 'none'
-      
-   })
-})
+   homebg.addEventListener('transitionend', () => {
+      homebg.style.display = 'none';
+   }, );
+});
